8df947e850a8cb16d18dbeb089603d98c7ad74c9
zlodij <ya-zlodij@yandex.ua>
2020-03-12 13:51:10 +0200

    Handlers for view/edit rule added

--- SpringMVC/src/main/java/com/example/permits/controller/RuleController.java
+++ SpringMVC/src/main/java/com/example/permits/controller/RuleController.java
@@ -1,20 +1,24 @@
 package com.example.permits.controller;
 
+import com.example.permits.model.BaseObject;
 import com.example.permits.model.Rule;
 import com.example.permits.service.RuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Comparator;
+import javax.validation.Valid;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Controller
 public class RuleController {
     @Autowired
     private RuleService service;
+
+    private Map<String, BaseObject> cache = new HashMap<>();
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -26,4 +30,43 @@ public class RuleController {
         model.addAttribute("rules", service.getAll().stream().sorted(Comparator.comparing(Rule::getName)).collect(Collectors.toCollection(ArrayList::new)));
         return "/rules";
     } // end getRulesView
+
+    @GetMapping("/rules/new")
+    public String getNewRuleView(Model model) {
+        Rule rule = new Rule();
+        cache.put(rule.getId(), rule);
+
+        model.addAttribute("rule", rule);
+        return "redirect:/rules/" + rule.getId() + "/rule";
+    } // end getNewRuleView
+
+    @GetMapping("/rules/{id}/rule")
+    public String getEditRuleView(@PathVariable("id") String id, Model model) {
+        Rule rule = service.getRuleById(id);
+        if (rule == null && cache.containsKey(id)) {
+            BaseObject object = cache.get(id);
+            if (object instanceof Rule) {
+                rule = (Rule) object;
+            }
+        }
+        model.addAttribute("rule", rule);
+        return "editRule";
+    } // end getEditRuleView
+
+    @PostMapping("/rules/{id}/rule")
+    public String sendEditRuleForm(@PathVariable("id") String id,
+                                   @ModelAttribute @Valid Rule rule,
+                                   BindingResult bindingResult,
+                                   Model model) {
+        String result;
+
+        if (bindingResult.hasErrors()) {
+            result = "editRule";
+        } else {
+            service.addRule(rule);
+            result = "redirect:/rules";
+        }
+
+        return result;
+    } // end sendEditRuleForm
 } // end RuleController
