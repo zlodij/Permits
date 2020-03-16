96d949503cd3b931067c353e3e034f223e09eea1
zlodij <ya-zlodij@yandex.ua>
2020-03-16 18:46:19 +0200

    Extended with parent ID's setter/getter

--- SpringMVC/src/main/java/com/example/permits/model/BaseObject.java
+++ SpringMVC/src/main/java/com/example/permits/model/BaseObject.java
@@ -4,4 +4,8 @@ public interface BaseObject {
     String getId(); // end getId
 
     void setId(String id);// end setId
+
+    String getParentId(); // end getParentId
+
+    void setParentId(String id);// end setParentId
 } // end BaseObject
