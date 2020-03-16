1b3013b2a818f62550e67497bfa3d4e2cc824afb
zlodij <ya-zlodij@yandex.ua>
2020-03-16 18:40:56 +0200

    Constant 'NULL_ID' added

--- SpringMVC/src/main/java/com/example/permits/utils/IDGenerator.java
+++ SpringMVC/src/main/java/com/example/permits/utils/IDGenerator.java
@@ -1,6 +1,8 @@
 package com.example.permits.utils;
 
 public final class IDGenerator {
+    public final static String NULL_ID = "00000000";
+
     private static int lastId = 0;
 
     public synchronized static String generateID(String pattern) {
