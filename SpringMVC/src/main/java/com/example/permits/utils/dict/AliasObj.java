663c30edd867c2a73c565c992bdb913ba3fe4344
zlodij <ya-zlodij@yandex.ua>
2020-04-29 13:20:55 +0300

    Initial commit

---
+++ SpringMVC/src/main/java/com/example/permits/utils/dict/AliasObj.java
@@ -0,0 +1,15 @@
+package com.example.permits.utils.dict;
+
+/**
+ * Provides simple realization of inner Properties Bag to deserialize dictionaries data from JSON config with Gson.
+ */
+public class AliasObj {
+    /**
+     * Refers on "alias" field
+     */
+    String alias;
+    /**
+     * Refers on "service" field
+     */
+    boolean service;
+} // end AliasObj
