a0cfefcb90155e4cee9bed28d8a5d13e5189cb15
zlodij <ya-zlodij@yandex.ua>
2020-05-19 21:26:00 +0300

    Initial commit

---
+++ SpringMVC/src/main/java/com/example/permits/enums/converter/StringToPermitConverter.java
@@ -0,0 +1,10 @@
+package com.example.permits.enums.converter;
+
+import com.example.permits.enums.Permit;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToPermitConverter implements Converter<String, Permit> {
+    public Permit convert(String source) {
+        return Permit.getPermit(Integer.parseInt(source)).orElse(Permit.NONE);
+    } // end convert
+} // end StringToPermitConverter
