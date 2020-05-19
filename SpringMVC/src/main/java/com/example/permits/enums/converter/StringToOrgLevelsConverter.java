bae080fe3e7fb412f22e0722d6317c1340a62aa1
zlodij <ya-zlodij@yandex.ua>
2020-05-19 22:32:57 +0300

    Initial commit

---
+++ SpringMVC/src/main/java/com/example/permits/enums/converter/StringToOrgLevelsConverter.java
@@ -0,0 +1,11 @@
+package com.example.permits.enums.converter;
+
+import com.example.permits.enums.OrgLevel;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToOrgLevelsConverter implements Converter<String, OrgLevel> {
+    public OrgLevel convert(String source) {
+        return OrgLevel.getOrgLevel(source).orElseThrow(RuntimeException::new);
+    } // end convert
+} // end StringToOrgLevelsConverter
+
---
+++ SpringMVC/src/main/java/com/example/permits/enums/converter/StringToXPermitConverter.java
@@ -0,0 +1,10 @@
+package com.example.permits.enums.converter;
+
+import com.example.permits.enums.XPermit;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToXPermitConverter implements Converter<String, XPermit> {
+    public XPermit convert(String source) {
+        return XPermit.getXPermit(source).orElseThrow(RuntimeException::new);
+    } // end convert
+} // end StringToXPermitConverter
